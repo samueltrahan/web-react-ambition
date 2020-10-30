09e2239ffbd8a7fd57f63d818cd4d555cecd01f3
Samuel Trahan <samtrahan91@gmail.com>
2020-10-30 16:52:20 -0500

    Banner section create

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Landing from './components/Banner/Banner';
+import './App.css';
+
+export default function App() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './Banner.css';
+
+export default function Landing() {
+  return (
+    <>
+      <section className="major">
+        <div className="landing-section">
+          <header>
+            <h1>Ambition</h1>
+          </header>
+          <div className="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+              quasi quam sint reprehenderit eius vitae harum molestiae? Placeat
+              aliquid, impedit, deleniti sunt, ratione natus pariatur fugit
+              eveniet nesciunt sit explicabo.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
