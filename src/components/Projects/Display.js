ca24d381cdcb676a026d720e2f764ff746d6527c
Samuel Trahan <samtrahan91@gmail.com>
2020-10-31 21:48:32 -0500

    display styling and some opacity addED

--- src/components/Projects/Display.js
+++ src/components/Projects/Display.js
@@ -1,41 +1,67 @@
-import React from 'react';
-import pic01 from '../../assets/images/pic01.jpg';
-import pic02 from '../../assets/images/pic02.jpg';
-import pic03 from '../../assets/images/pic03.jpg';
-import pic04 from '../../assets/images/pic04.jpg';
-import './Display.css'
+import React from "react";
+import pic01 from "../../assets/images/pic01.jpg";
+import pic02 from "../../assets/images/pic02.jpg";
+import pic03 from "../../assets/images/pic03.jpg";
+import pic04 from "../../assets/images/pic04.jpg";
+import "./Display.css";
 
 export default function Display() {
   return (
     <div className="minor">
-      <section className="idea" >
-        <article className="idea-1" style={{backgroundImage: `url(${pic01})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', width: '100%', height: '100%'}}>
-        <div>
-          <h1>Project 1</h1>
-        </div>
+      <section className="idea">
+        <article
+          className="idea-1"
+          style={{
+            backgroundImage: `url(${pic01})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <header>
+            <h1>Project 1</h1>
+          </header>
         </article>
       </section>
       <section className="idea">
-        <article style={{backgroundImage: `url(${pic02})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-        <div className="idea-2">
-          <h1>Project 2</h1>
-        </div>
+        <article
+          className="idea-2"
+          style={{
+            backgroundImage: `url(${pic02})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <header>
+            <h1>Project 2</h1>
+          </header>
         </article>
       </section>
       <section className="idea">
-        <article style={{backgroundImage: `url(${pic03})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-        <div className="idea-3">
-          <h1>Project 3</h1>
-        </div>
+        <article
+          className="idea-3"
+          style={{
+            backgroundImage: `url(${pic03})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <header>
+            <h1>Project 3</h1>
+          </header>
         </article>
       </section>
       <section className="idea">
-        <article style={{backgroundImage: `url(${pic04})`}}>
-        <div className="idea-4">
-          <h1>Project 4</h1>
-        </div>
+        <article
+          className="idea-4"
+          style={{ backgroundImage: `url(${pic04})` }}
+        >
+          <header>
+            <h1>Project 4</h1>
+          </header>
         </article>
       </section>
     </div>
-  )
+  );
 }
