03e4cc795cc26f3abdc14d2308d0e5a22bae90b2
Samuel Trahan <samtrahan91@gmail.com>
2020-11-01 19:35:17 -0600

    contact component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Landing from './components/Banner/Banner';
 import Header from './components/Header/Header';
 import Display from './components/Projects/Display';
+import Contact from './components/Contact/Contact';
 import './App.css';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <Landing />
       <Display />
+      <Contact />
     </div>
   )
 }
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Contact.css'
+
+export default function Contact() {
+  return (
+    <>
+      <section className="contact">
+        <div className="inner">
+
+        </div>
+      </section>
+    </>
+  )
+}
